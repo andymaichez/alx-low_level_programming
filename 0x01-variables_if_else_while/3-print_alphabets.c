526275e6a95c80281f84710e7463e9f3198a53f2
andymaichez <andymaichez@gmail.com>
2023-02-17 02:49:24 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
  * main -  program that prints the alphabet in lowercase, and then in uppercase,
  * followed by a new line.
- * Return: Always 0 (Success)
+ * Return:  0 (Success)
  */
 int main(void)
 {
