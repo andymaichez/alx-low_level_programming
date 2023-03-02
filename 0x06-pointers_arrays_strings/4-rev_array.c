890c668496be829bd2edeb4dfb479d1774329e60
andymaichez <andymaichez@gmail.com>
2023-03-02 02:05:43 -0800

    am a kind of paranoid in reverse.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,19 +10,18 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
+ int i;
+ i = 0;
+ while (i < n)
+ {
+ if (i != 0)
+ {
+  printf(", ");
+ }
+  printf("%d", a[i]);
+  i++;
+ }
+  printf("\n");
 }
 
 /**
