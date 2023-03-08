6dd642fed6023af6a9ebdf995397e1d41e74dc4f
andymaichez <andymaichez@gmail.com>
2023-03-08 03:16:22 -0800

    she locked away a secret

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,10 @@
-#ifndef MAIN_H #define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
 /**
 * File: main.h
-* Auth: me
-* Desc: Header file containing prototypes for all functions * used in the 0x07-recursion directory.
+* Auth: andrew
+* Desc: Header file containing prototypes for all functions
 */
 int _putchar(char c);
 void _puts_recursion(char *s);
