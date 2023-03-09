47abd32907fecce0b0a861d4de728ddc8be1c456
andymaichez <andymaichez@gmail.com>
2023-03-09 01:31:46 -0800

    dream a little bigger

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,8 @@
 /*
-*3-factorial.c  
-*Auth: andrew
+* 3-factorial.c * Auth: andrew
 */
 #include "main.h"
+#include <stdio.h>
 /**
 * factorial - Returns the factorial of a given number.
 * @n: The number to find the factorial of. *
@@ -16,7 +16,7 @@ int result = n;
 if (n < 0)
 return (-1);
 else if
-(n >= 0& n <= 1) return (1);
+(n >= 0 && n <= 1) return (1);
 result *= factorial(n - 1);
 return (result);
 }
