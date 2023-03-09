12ccbaefbb62397b79d0095bd99b2acd88687d0b
andymaichez <andymaichez@gmail.com>
2023-03-09 02:42:01 -0800

    is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+/*
+* File: 6-is_prime_number.c
+*/
+#include "main.h"
+#include <stdio.h>
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+/**
+* is_divisible - Checks if a number is divisible.
+* @num: The number to be checked.
+* @div: The divisor. *
+* Return: If the number is divisible - 0.
+*/
+int is_divisible(int num, int div)
+{
+if (num % div == 0)
+return (0);
+if (div == num / 2) 
+return (1);
+return (is_divisible(num, div + 1));
+}
