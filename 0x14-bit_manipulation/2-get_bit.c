dcf445ffbc3a76409229cd72176d215cb27610a3
andymaichez <andymaichez@gmail.com>
2023-04-09 12:19:32 -0700

    returns the value of a bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,13 +8,11 @@
  * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
-unsigned long int mask;
 {
 if (index >= sizeof(unsigned long int) * 8)
 {
-return (-1);
+return (-1); /* Error: index out of range*/
 }
-mask = 1UL << index;
-return (n & mask != 0);
+return ((n >> index) & 1);
 }
 
