7bbae1ea833832f4a4ab5cc97f8d8795bfbafcd3
andymaichez <andymaichez@gmail.com>
2023-04-09 23:11:04 -0700

    reads a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,36 +4,52 @@
 #include <unistd.h>
 
 /**
- * main - checks the main file
- * read_textfile - reads a text file and prints it
- * @letters : number of letters it should read and print
- * @filename: file being read
- * Return: actual number of letters it could read and print
- * 0 when the file fails, NULL or  can'tbe opened
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: name of the file to read.
+ * @letters: number of letters it should read and print.
+ * Return: the actual number of letters it could read and print,
+ * or 0 if failed.
  */
-int main(void)
-ssize_t read_textfile(const char *filename, size_t letters);
+
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file = fopen(filename, "r");
+int fd;
+ssize_t bytes_read, bytes_written;
+char *buffer;
 
-	if (file == NULL)
-	return (0);
+buffer = malloc((letters + 1) * sizeof(char));
+if (!buffer)
+return (0);
 
-	char *buffer = (char *) malloc(letters * sizeof(char));
-
-	if (buffer == NULL)
-	{
-		return (0);
-	}
-	ssize_t bytes_read = fread(buffer, sizeof(char), letters, file);
-
-	fclose(file);
-
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	free(buffer);
-
-	if (bytes_written != bytes_read)
-	return (0);
-	return (bytes_written);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+free(buffer);
+return (0);
 }
+
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+buffer[bytes_read] = '\0';
+
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+free(buffer);
+close(fd);
+return (bytes_written);
+}
+
+
