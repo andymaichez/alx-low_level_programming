e9ce750742707b927fed8b3676d520b7986d5b5f
andymaichez <andymaichez@gmail.com>
2023-04-09 23:28:53 -0700

    appends text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,42 +1,44 @@
 #include "main.h"
 #include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 /**
- * main - creates the function file
- * create_file - Creates a file
- * @text_content: writes to a file
- * @filename: name of the file created
- * Return: 1 on success and -1 on failure
- */
-
-int create_file(const char *filename, char *text_content)
+* append_text_to_file - appends the given text to the
+* end of the given file.
+* @filename: name of the file to append to.
+* @text_content: null-terminated string to append to the file.
+* Return: 1 on success, -1 on failure.
+*/
+int append_text_to_file(const char *filename, char *text_content)
 {
+int fd, bytes_written, len = 0;
+
 if (filename == NULL)
 return (-1);
 
-int fd;
-ssize_t bytes_written;
-size_t text_len = 0;
+if (text_content != NULL)
+{
+while (text_content[len])
+len++;
+}
 
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 
 if (text_content != NULL)
 {
-while (text_content[text_len] != '\0')
-text_len++;
-
-bytes_written = write(fd, text_content, text_len);
-if (bytes_written == -1)
+bytes_written = write(fd, text_content, len);
+if (bytes_written == -1 || bytes_written != len)
 {
 close(fd);
 return (-1);
 }
 }
+
 close(fd);
 return (1);
 }
+
